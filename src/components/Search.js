ed5b2b68d3d11c900f6bbc97cdeec040bf78b770
kwu3 <kwu3@villanova.edu>
2020-05-27 16:04:15 -0400

    Centered search bar, removed button

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,13 +11,19 @@ import React from "react";
 
 function Search() {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav style={styles.searchBar} className="navbar navbar-light bg-light">
       <form className="form-inline">
         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
     </nav>
   );
 }
 
 export default Search;
+
+const styles = {
+  searchBar: {
+    display: "flex",
+    justifyContent: "center",
+  }
+}
