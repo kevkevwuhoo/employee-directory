d5e990430cba58e163945f63789b9acd14dd6159
kwu3 <kwu3@villanova.edu>
2020-05-27 15:33:01 -0400

    Created react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import "./App.css";
+import Header from "./components/Header";
+
+// main container
+// header
+// search
+// table
+//
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
