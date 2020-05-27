87b04dd3a9c8c7a508205f479a66cb17050e1d0c
kwu3 <kwu3@villanova.edu>
2020-05-27 15:34:34 -0400

    Created header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">Say hello to your team.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
